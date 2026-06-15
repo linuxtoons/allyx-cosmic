2f32b1929c6d1d7c4f6219791acb96730983ba68
Max <118260621+linuxtoons@users.noreply.github.com>
2026-06-15 15:12:53 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,15 +2,14 @@
 
 set -ouex pipefail
 
-### Install packages
+
+
+dnf remove -y plasma-*
 
 dnf copr enable -y ryanabx/cosmic-epoch
 dnf install -y cosmic-desktop
 dnf copr disable -y ryanabx/cosmic-epoch
 
-dnf remove -y plasma-*
-
-#### Example for enabling a System Unit File
 
 systemctl enable podman.socket
 systemctl enable cosmic-greeter
