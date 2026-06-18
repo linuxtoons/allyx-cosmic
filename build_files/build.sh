86774edeab2a95d0aca63d8e1857e4b8d1b4d2df
Max <118260621+linuxtoons@users.noreply.github.com>
2026-06-18 15:28:30 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,7 @@ dnf copr disable -y ryanabx/cosmic-epoch
 
 wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.4-1.noarch.rpm"
 dnf install -y ./protonvpn-stable-release-1.0.4-1.noarch.rpm
+dnf install -y proton-vpn-gnome-desktop
 
 
 systemctl enable cosmic-greeter
